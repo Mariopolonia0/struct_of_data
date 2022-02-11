2e7d699156fce5dff59571c6f0b3614a1fd4f594
Mario Peña <65502311+Mariopolonia0@users.noreply.github.com>
2022-02-11 13:58:13 -0400

    Update ejer3.cpp

--- Entregable 1/ejer3.cpp
+++ Entregable 1/ejer3.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(){
+		
 	string cadena1="123654547899542586545646546546465468835468468464646848454878464";
 	string cadena2="21548755555484879879874968468468468465464648468464865468468468684";
 	cout<<"\n\n\t\tEsta Es La Suma De Las Dos Cadenas: ";
@@ -10,14 +11,9 @@ int main(){
 	double numero2=0;
 	int i=0;
 	while ( i < cadena1.length() )
-  {
-    numero1=numero1+cadena1[i];
-    numero2=numero2+cadena2[i];++i;
-  }
-   cout<<numero1+numero2*100;
-	
-
-
-	
-	
+	{
+	    numero1=numero1+cadena1[i];
+	    numero2=numero2+cadena2[i];++i;
+	}
+   cout<<numero1+numero2*100;	
 }
