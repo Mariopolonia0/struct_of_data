bdb81383a72d99c161c7331a2606348da480b692
Mario <mariopolonia0@gmail.com>
2022-11-09 11:51:04 -0400

    agregando ejercicio de invertir oracion en kotlin

---
+++ EjecicioKotlin/InvertirOracion.kt
@@ -0,0 +1,29 @@
+package EjecicioKotlin
+
+fun main() {
+
+    invertirOracion("Mario Polonia")
+}
+
+fun invertirOracion( oracion : String){
+
+    var resultado = ""
+
+    val oracionDividida = oracion.split(" ")
+
+    for (veces in 0..(oracionDividida.size - 1)) {
+
+        var contador = oracionDividida[veces].length -1
+
+        while (contador >= 0){
+            resultado += oracionDividida[veces].get(contador)
+            contador--
+        }
+
+        resultado += " "
+    }
+
+    println(oracion)
+    println(resultado)
+}
+
