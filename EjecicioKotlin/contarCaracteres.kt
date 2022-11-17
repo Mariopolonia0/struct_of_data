fef8496cbcdf4e4a255b2893b35f3e0c3021cfe5
Mario <mariopolonia0@gmail.com>
2022-11-17 11:07:32 -0400

    agregando ejercicio 2.5 y 2.6 en kotlin del libro de fundamento de la programacion

---
+++ EjecicioKotlin/contarCaracteres.kt
@@ -0,0 +1,63 @@
+package EjecicioKotlin
+
+fun main() {
+    println(sumaTablaTres())
+    cantidadCaracter("abcdefd")
+    println(mayorCuatroNumero(5, 6, 9, 8))
+    println(mayorCuatroNumero(8, 10, 6, 2))
+}
+//2.5. Diseñar un algoritmo que visualice y sume la serie de
+//números 3, 6, 9, 12…, 99.
+private fun sumaTablaTres(): Double {
+    var suma = 0.0
+
+    for (contador in 1..33) {
+        suma += (3 * contador)
+    }
+
+    return suma
+}
+
+//2.6. Escribir un algoritmo que lea cuatro números
+// y a continuación visualice el mayor de los cuatro.
+private fun mayorCuatroNumero(numeroUno: Int, numeroDos: Int, numeroTres: Int, numeroCuatro: Int): Int {
+    var numeroMayor = 0
+
+    if (numeroMayor < numeroUno) {
+        numeroMayor = numeroUno
+    }
+
+    if (numeroMayor < numeroDos) {
+        numeroMayor = numeroDos
+    }
+
+    if (numeroMayor < numeroTres) {
+        numeroMayor = numeroTres
+    }
+
+    if (numeroMayor < numeroCuatro) {
+        numeroMayor = numeroCuatro
+    }
+
+    return numeroMayor
+}
+
+//    "abcdefd"
+//    [1, 1, 1, 2, 1, 1]
+private fun cantidadCaracter(cadena: String) {
+    val cantidadLetras = mutableListOf<Int>()
+    var contador = 0
+    while (contador < cadena.length - 1) {
+        var contador2 = contador + 1
+        var numeroLetra = 1
+
+        while (contador2 < cadena.length) {
+            if (cadena[contador] == cadena[contador2])
+                numeroLetra++
+            contador2++
+        }
+        cantidadLetras.add(numeroLetra)
+        contador++
+    }
+    println(cantidadLetras)
+}
--- EjecicioKotlin/ejercicioPruebaOrionTek.kt
+++ EjecicioKotlin/ejercicioPruebaOrionTek.kt
@@ -15,7 +15,51 @@ import java.util.*
 
 fun main() {
     val numbers = Arrays.asList(1, 4, 5, 0, 5)
-    pairOfSumToTarget(numbers, 5).forEach{ println(it) }
+    val numberos = arrayOf(1, 5, 6, -2, -9, 6, 0,5)
+    determinarPasitivosNegativos(numberos)
+    pairOfSumToTarget(numbers, 5).forEach { println(it) }
+    println(sumarLista(numbers))
+    quitarNumeroRepetido(numberos)
+}
+
+private fun quitarNumeroRepetido(numeros: Array<Int>) {
+    var contador = 0
+
+    while (contador < numeros.size) {
+        var contador2 = 0
+
+        while (contador2 < numeros.size) {
+            if (contador != contador2){
+                if (numeros[contador2] == numeros[contador])
+                    numeros[contador2] = 0
+            }
+            contador2++
+        }
+        contador++
+    }
+
+    for (item in numeros)
+        println(item)
+}
+
+private fun determinarPasitivosNegativos(listaNumero: Array<Int>) {
+
+    for (item in listaNumero)
+        if (item >= 0)
+            println("$item es positivo")
+        else
+            println("$item es negativo")
+}
+
+private fun sumarLista(numbers: List<Int>): Int {
+    var contador = 0
+    var suma = 0
+
+    while (contador < numbers.size) {
+        suma += numbers[contador]
+        contador++
+    }
+    return suma
 }
 
 private fun pairOfSumToTarget(numbers: List<Int>, target: Int): List<String?> {
