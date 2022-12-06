f431fb18cf94b79195c7c0bfe24bed295bad3bc6
Mario Peña Polonia <65502311+Mariopolonia0@users.noreply.github.com>
2022-12-06 15:34:37 -0400

    Update ejercicio2_6.kt
    agregando ejercicio 💯  2.11. Calcular y visualizar la longitud de la circunferencia 
    y el área de un círculo de radio dado.

--- EjecicioKotlin/ejercicio2_6.kt
+++ EjecicioKotlin/ejercicio2_6.kt
@@ -1,5 +1,8 @@
 package EjecicioKotlin
 
+import kotlin.math.pow
+import kotlin.math.round
+
 //Diseñar un algoritmo para calcular la velocidad (en
 //m/s) de los corredores de la carrera de 1.500 metros.
 //La entrada consistirá en parejas de números (minutos,
@@ -18,11 +21,27 @@ fun main() {
     println(calcularVelocidad(3.53f,1500))
     print("Tiempo era de 3.40 m/s y su velocidad fue de ")
     println(calcularVelocidad(3.40f,1500))
+    
+    longitudAreaCircunferencia(8.0)
+    longitudAreaCircunferencia(90.0)
+    longitudAreaCircunferencia(5.5)
+    longitudAreaCircunferencia(6.1)
 }
 
 private fun calcularVelocidad(tiempo: Float, metro: Int): String {
     return (metro/tiempo).toString()
 }
 
-
-
+//LONGITUD de la CIRCUNFERENCIA = 2 · radio · π 
+//ÁREA del CÍRCULO = π · radio al cuadrado = π · r2 
+ 
+private fun longitudAreaCircunferencia(radio : Double){
+    
+    var longitud = 2 * radio * 3.14
+    val area = Math.round((3.14 * (radio.pow(2) )))
+    
+    println("El radio de la circunferencia es $radio")
+    println("La logitud de la circunferencia es $longitud")
+    println("El área de la circunferencia es $area")
+    println("--------------------------------------------")
+}
