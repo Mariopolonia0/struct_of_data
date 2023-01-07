5b261dfa74f0abef2e5fafdbbc5586cf384f184a
Mario <mariopolonia0@gmail.com>
2023-01-07 10:05:31 -0400

    agregando ejecicios del Ejercicio 5.8

--- EjercicioKotlin/capitulo5.kt
+++ EjercicioKotlin/capitulo5.kt
@@ -2,9 +2,40 @@ package EjercicioKotlin
 
 fun main() {
     //  ejercicio5_5()
-    ejercicio5_6()
+    ejercicio5_8()
+    // ejercicio5_6()
 //    println(ejercicio5_1(lista = listOf(6, 11, 7)).toString())
 //    println(ejercicio5_1(lista = listOf(3, 7, 5, 13, 20, 23, 39, 23, 40, 23, 14, 12, 56, 23, 29)).toString())
+}
+
+//Ejercicio 5.8
+//Leer 100 números. Determinar la media de los números positivos
+//y la media de los números negativos.
+private fun ejercicio5_8() {
+    var cantidadNumero = 0
+    var cantidadNumeroNegativo = 0
+    var sumaNumero = 0
+    var sumaNumeroNegativo = 0
+
+    for (contador in 1..10) {
+
+        print("Digite Un Numero:")
+        val numeroDigitado = readln().toInt()
+
+        if (numeroDigitado >= 0) {
+            cantidadNumero++
+            sumaNumero += numeroDigitado
+        } else {
+            cantidadNumeroNegativo++
+            sumaNumeroNegativo += numeroDigitado
+        }
+    }
+
+    val mediaNumeroPositivo = sumaNumero / cantidadNumero
+    val mediaNumeroNegativo = sumaNumeroNegativo / cantidadNumeroNegativo
+
+    println("\nMedia de numero positivo: $mediaNumeroPositivo")
+    println("\nMedia de numero negativo: $mediaNumeroNegativo")
 }
 
 //Determinar la media de una lista indefinida de números
