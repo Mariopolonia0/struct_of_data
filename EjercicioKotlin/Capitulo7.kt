257a15090ea3f57ebc163b93bdb17a23f6d129be
Mario <mariopolonia0@gmail.com>
2023-02-03 12:10:47 -0400

    agregando ejercicio 7.5  Cálculo de la suma de todos los elementos de un vector, así como la media aritmética

--- EjercicioKotlin/Capitulo7.kt
+++ EjercicioKotlin/Capitulo7.kt
@@ -1,11 +1,22 @@
 package EjercicioKotlin
 
 fun main() {
+    ejercicio7_5(arrayOf(4, 6, 9, 8, 5, 6, 2))
+    ejercicio7_5(arrayOf(14, 16, 12, 12, 10, 18,20,14))
     //ejercicio7_1()
-//    ejercicio7_2(listaNumero = arrayOf(1, 55, -6, 66, -85, 0, 0))
-//    println("\n----------------")
-//    ejercicio7_3()
-    ejercicio7_4()
+    //ejercicio7_2(listaNumero = arrayOf(1, 55, -6, 66, -85, 0, 0))
+    //println("\n----------------")
+    //ejercicio7_3()
+    //ejercicio7_4()
+}
+
+// Cálculo de la suma de todos los elementos de un vector, así como la media aritmética
+private fun ejercicio7_5(array: Array<Int>) {
+    var suma = 0
+    for (item in array) {
+        suma += item
+    }
+    println("la suma es $suma y la media aritmética ${suma / array.size}")
 }
 
 /*
