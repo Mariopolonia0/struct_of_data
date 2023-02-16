7a146fc93fdef77efe9e4517c99682fc22ee986c
Mario Peña Polonia <65502311+Mariopolonia0@users.noreply.github.com>
2023-02-16 10:26:24 -0400

    Update ejercio_7_tarea_1.c

--- C/Tarea I/ejercio_7_tarea_1.c
+++ C/Tarea I/ejercio_7_tarea_1.c
@@ -1,17 +1,23 @@
-#include<stdio.h>//hola
-//hola
+#include<stdio.h>
+
+//Hacer un algoritmo que pida tres números y calcule su promedio.
+
 main(){
 	
 	float n1,n2,n3,total;
-    printf("este programa le dira el promedio de tres numeros\n\n");	
-	printf("digite el primer  numero : ");scanf("%f",&n1);
-	printf("digite el segundo numero : ");scanf("%f",&n2);	
-	printf("digite el tercero numero : ");scanf("%f",&n3);
-	total=(n1+n2+n3)/3;printf("\n%.2f es el promedio de los tres numeros\n\n",total);
-		
-	//hola profe soy mario 
+        printf("este programa le dira el promedio de tres numeros\n\n");
+	
+	printf("digite el primer  numero : ");
+	scanf("%f",&n1);
+	
+	printf("digite el segundo numero : ");
+	scanf("%f",&n2);	
+	
+	printf("digite el tercero numero : ");
+	scanf("%f",&n3);
+	
+	total = (n1 + n2 +n3 ) / 3; 
+	
+	printf("\n%.2f es el promedio de los tres numeros\n\n",total);
 	system("pause");
-	    //hola
- 	//hola
-//hola	
 }
