26fea6fc6919811256e0aa307d7dca2d833678e6
Mario <mariopolonia0@gmail.com>
2023-02-16 20:33:05 -0400

    bueno

--- EjercicioJava/programacion3/ejercicioHechoEnClase/Realizado_20_9_2019/Cuadrado.java
+++ EjercicioJava/programacion3/ejercicioHechoEnClase/Realizado_20_9_2019/Cuadrado.java
@@ -26,7 +26,6 @@ public class Cuadrado {
         }
     }
 
-
     float getArea() {
         return lado * lado;
     }
--- EjercicioKotlin/HackerRank/Algorithms_Warmup_A_Very_Big_Sum.kt
+++ EjercicioKotlin/HackerRank/Algorithms_Warmup_A_Very_Big_Sum.kt
@@ -2,7 +2,6 @@ package EjercicioKotlin.HackerRank
 
 /*
  * Complete the 'diagonalDifference' function below.
- *
  * The function is expected to return an INTEGER.
  * The function accepts 2D_INTEGER_ARRAY arr as parameter.
  */
