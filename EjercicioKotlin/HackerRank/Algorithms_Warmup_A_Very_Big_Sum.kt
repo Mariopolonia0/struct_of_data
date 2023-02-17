26f7b7f045f52efce5fbc66f590bda753fc673d4
Mario <mariopolonia0@gmail.com>
2023-02-16 20:11:36 -0400

    Agregando ejercicio HackerRank diagonal Difference sumando las dialogales de una matriz y luego restaba las sumas de las diagonales y el resultado tiene que ser positivo o sea que al final si es negativo hay que multiplicarlo por -1

--- EjercicioKotlin/HackerRank/Algorithms_Warmup_A_Very_Big_Sum.kt
+++ EjercicioKotlin/HackerRank/Algorithms_Warmup_A_Very_Big_Sum.kt
@@ -1,12 +1,60 @@
 package EjercicioKotlin.HackerRank
 
 /*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+fun diagonalDifference(arr: Array<Array<Int>>): Int {
+    // Write your code here
+    val suma = Array(2) { 0 }
+
+    var contador = 0
+    var contador2 = arr.size - 1
+
+    while(contador < arr.size){
+        suma[0] += arr[contador][contador]
+        contador++
+    }
+
+    contador = 0
+    while(contador < arr.size){
+        suma[1] += arr[contador][contador2]
+        contador2--
+        contador++
+    }
+
+    val result = (suma[0] - suma[1])
+
+    return if(result >= 0){
+        result
+    }else{
+        result * -1
+    }
+}
+
+fun main() {
+    val n = readLine()!!.trim().toInt()
+
+    val arr = Array<Array<Int>>(n, { Array<Int>(n, { 0 }) })
+
+    for (i in 0 until n) {
+        arr[i] = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+    }
+
+    val result = diagonalDifference(arr)
+
+    println(result)
+}
+
+/*
  * Complete the 'aVeryBigSum' function below.
  *
  * The function is expected to return a LONG_INTEGER.
  * The function accepts LONG_INTEGER_ARRAY ar as parameter.
  */
-
 fun aVeryBigSum(ar: Array<Long>): Long {
     // Write your code here
     var suma : Long = 0
@@ -18,10 +66,10 @@ fun aVeryBigSum(ar: Array<Long>): Long {
     return suma
 }
 
-fun main() {
-    val ar = readLine()!!.trimEnd().split(" ").map{ it.toLong() }.toTypedArray()
-
-    val result = aVeryBigSum(ar)
-
-    println(result)
-}
+//fun main() {
+//    val ar = readLine()!!.trimEnd().split(" ").map{ it.toLong() }.toTypedArray()
+//
+//    val result = aVeryBigSum(ar)
+//
+//    println(result)
+//}
