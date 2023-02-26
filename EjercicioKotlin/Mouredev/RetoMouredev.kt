e1be72f6a3b2a60c860d90cc359cf2868b95fb6f
Mario <mariopolonia0@gmail.com>
2023-02-26 17:15:30 -0400

    agregandole ejercicio mouredev

--- EjercicioKotlin/Mouredev/RetoMouredev.kt
+++ EjercicioKotlin/Mouredev/RetoMouredev.kt
@@ -1,8 +1,34 @@
 package EjercicioKotlin
 
 fun main() {
-    reto2()
+    //reto2()
+    ramdow1_100()
 }
+
+fun ramdow1_100() {
+
+    val time = System.currentTimeMillis().toString()
+    var semilla = time.substring(time.length - 3, time.length).toInt()
+    var hasta = 0
+    var numero = 0.0
+    var numeroDos = 0.0
+    var contador = 0
+
+    print("Cuanto número aleatorio se imprimirán:")
+    hasta = readLine()!!.toInt()
+
+
+    while (contador < hasta) {
+        numero = ((5 * semilla + 7) % 8).toDouble()
+        numeroDos = numero * 8.toDouble()
+        if (numeroDos > 0 && numeroDos <= 100){
+            println("$numeroDos")
+            contador ++
+        }
+        semilla = numero.toInt()
+    }
+}
+
 
 private fun reto2() {
     var puntoPlayerUno = 0
@@ -19,7 +45,7 @@ private fun reto2() {
         Thread.sleep(2000)
     } while (puntoPlayerDos < 3 && puntoPlayerUno < 3)
 
-    if (puntoPlayerUno == puntoPlayerDos){
+    if (puntoPlayerUno == puntoPlayerDos) {
         println("\n Empate")
         return
     }
