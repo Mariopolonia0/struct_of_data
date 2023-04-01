a603016c82e831296d7f5d8683d99328408c38c1
Mariopolonia0 <mariopolonia0@gmail.com>
2023-04-01 17:31:46 -0400

    agregando los campos

--- javaScriptBasico/ejercicio9_5/main.js
+++ javaScriptBasico/ejercicio9_5/main.js
@@ -1,18 +1,37 @@
-class Estudiante {
-    constructor(nombre, calificacion) {
-        this.nombre = nombre,
-        this.calificacion = calificacion
+class Venta {
+    constructor(cliente, tipoVenta) {
+        this.cliente = cliente,
+            this.tipoVenta = tipoVenta
     }
 }
 
-function probando() {
-    var data = document.getElementById("input").value
-    var estudiante = new Estudiante(data, 500);
-    localStorage.setItem('a', JSON.stringify(estudiante));
+class Compra {
+    constructor(suplidor, empresa) {
+        this.suplidor = suplidor,
+            this.empresa = empresa
+    }
 }
 
-function printLocal() {
-    var klk = JSON.parse(localStorage.getItem('a'));
-    alert(klk.nombre);
+function SaveVenta() {
+    var _cliente = document.getElementById("cliente").value
+    var _tipoVenta = document.getElementById("tipoVenta").value
+    var venta = new Venta(_cliente, _tipoVenta);
+    localStorage.setItem('venta', JSON.stringify(venta));
 }
 
+function PrintVenta() {
+    var venta = JSON.parse(localStorage.getItem('venta'));
+    alert(venta.cliente + " " + venta.tipoVenta);
+}
+
+function SaveCompra() {
+    var _suplidor = document.getElementById("suplidor").value
+    var _empresa = document.getElementById("empresa").value
+    var compra = new Compra(_suplidor, _empresa);
+    localStorage.setItem('compra', JSON.stringify(compra));
+}
+
+function PrintCompra() {
+    var compra = JSON.parse(localStorage.getItem('compra'));
+    alert(compra.suplidor + " " + compra.empresa);
+}
