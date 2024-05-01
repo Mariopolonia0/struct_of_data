b7dcd3ac3bb6bebd8c71f2be4d148e0ec441a772
mario <mariopolonia0@gmil.com>
2024-04-30 20:32:51 -0400

    agregando codigo del led rgb  de la placa china WisdPi Tiny RP2040

--- raspberry_pi_pico/wisdpi tiny rp2040 motor.py
+++ raspberry_pi_pico/wisdpi tiny rp2040 motor.py
@@ -8,16 +8,11 @@ pixel = neopixel.NeoPixel(machine.Pin(pixel_pin), 1)
 pixel[0] = (0, 17, 255)
 pixel.write()
 
-# led = machine.Pin(20,machine.Pin.OUT)
-# led1 = machine.Pin(19,machine.Pin.OUT)
-# led2 = machine.Pin(18,machine.Pin.OUT)
-# led3 = machine.Pin(17,machine.Pin.OUT)
+leftBack = machine.Pin(17,Pin.OUT)
+leftForward = machine.Pin(18,Pin.OUT)
 
-leftBack = machine.Pin(20,Pin.OUT)
-leftForward = machine.Pin(19,Pin.OUT)
-
-rigthForward = machine.Pin(18,Pin.OUT)
-righBack = machine.Pin(17,Pin.OUT)
+rigthForward = machine.Pin(20,Pin.OUT)
+righBack = machine.Pin(19,Pin.OUT)
 
 leftForward.value(0)
 leftBack.value(0)
@@ -25,8 +20,12 @@ leftBack.value(0)
 rigthForward.value(0)
 righBack.value(0)
 
+pixel[0] = (0, 0, 0)
+pixel.write()
+
 uart = UART(0,9600)
 
+#prueba
 while True:
     if uart.any() > 0:
         dato = uart.readline()
@@ -42,17 +41,28 @@ while True:
 
         if "1" in dato:
             leftForward.value(1)
-            rigthForward.value(1)
             leftBack.value(0)
+            rigthForward.value(1)
             righBack.value(0)
-            pixel[0] = (0, 0, 255)
-            pixel.write()
-
-        if "2" in dato:
-            leftBack.value(1)
-            righBack.value(1)
-            rigthForward.value(0)
-            leftForward.value(0)
             pixel[0] = (255, 0, 0)
             pixel.write()
 
+        if "2" in dato:
+            leftForward.value(0)
+            leftBack.value(1)
+            rigthForward.value(0)
+            righBack.value(1)
+            pixel[0] = (0, 0, 255)
+            pixel.write()
+
+        if "3" in dato:
+            leftForward.value(0)
+            leftBack.value(1)
+            rigthForward.value(1)
+            righBack.value(0)
+
+        if "4" in dato:
+            leftForward.value(0)
+            leftBack.value(1)
+            rigthForward.value(0)
+            righBack.value(1)
