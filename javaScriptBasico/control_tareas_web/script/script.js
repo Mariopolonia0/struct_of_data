c59564265e806312d7303a19f41466bbae86b833
mario <mariopolonia0@gmil.com>
2024-06-02 14:21:38 -0400

    agregando web control tarea

--- javaScriptBasico/control_tareas_web/script/script.js
+++ javaScriptBasico/control_tareas_web/script/script.js
@@ -1,9 +1,34 @@
 function login() {
 
-    const uri = 'http://www.apinomina.somee.com';
+    username
 
+    const uri = 'http://www.apinomina.somee.com/api/Books';
     fetch(uri)
         .then(response => console.log(response.json()))
-        .then(data => console.log(data))
-        .catch(error => console.error('Unable to get items.', error));
+        .then(data => document.getElementById("username").value = data)
+        .catch(error => document.getElementById("username").value = error);
+
+
+
+
+
+
+    /* headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
+     headers.append('Access-Control-Allow-Credentials', 'true');
+ 
+     headers.append('GET', 'POST', 'OPTIONS');
+ 
+     headers.append('Authorization', 'Basic ' + base64.encode(username + ":" + password));
+ 
+     fetch(sign_in, {
+         //mode: 'no-cors',
+         credentials: 'include',
+         method: 'POST',
+         headers: headers
+     })
+         .then(response => response.json())
+         .then(json => console.log(json))
+         .catch(error => console.log('Authorization failed : ' + error.message));
+ 
+     */
 }
