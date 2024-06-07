169e9b6ffbe77d7ea679b42b31188131d11c3e04
mario <mariopolonia0@gmil.com>
2024-06-06 22:16:11 -0400

    agregando el terminado conectado a la api

--- javaScriptBasico/control_tareas_web/script/scriptIndex.js
+++ javaScriptBasico/control_tareas_web/script/scriptIndex.js
@@ -3,37 +3,50 @@ function login() {
     const nombreUsuarioIngresado = document.getElementById('nombreUsuario').value
     const passwordIngresado = document.getElementById('password').value
 
-    if (nombreUsuarioIngresado == '' || passwordIngresado == '') {
+  /*  if (nombreUsuarioIngresado == '' || passwordIngresado == '') {
         alert("Campos estan vacio")
         return
     }
+    
+    */
 
     const data = {
         nombreUsuario: nombreUsuarioIngresado,
         password: passwordIngresado
     };
 
-    const uri = "http://www.apinomina.somee.com/Usuario/login/";
+    //const uri = "http://www.apinomina.somee.com/Usuario/login/";
+    const uri = 'http://www.apinomina.somee.com/api/Books';
 
-    fetch(uri, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data)
-    })
-        .then((res) => res.json())
+    fetch(uri)
+        .then((response) => response.json())
         .then(function (dataObject) {
-alert(dataObject.usuarioId)
-            if (dataObject.usuarioId == 0) {
-                alert("login incorrecto")
-            } else {
-                window.location = "ui/listaTarea.HTML";
-            }
+            console.log(dataObject[0].bookId)
+            alert(dataObject[0].nombre)
+        })
+        .catch(error => document.getElementById("username").value = error);
 
-        })
-        .catch((error) => {
-            console.log(error)
-            alert("Tenemos un error en el Server")
-        })
+    /* fetch(uri, {
+         method: "POST",
+         headers: {
+             "Content-Type": "application/json",
+         },
+         body: JSON.stringify(data)
+     })
+         .then((res) => res.json())
+         .then(function (dataObject) {
+ 
+             alert(dataObject.usuarioId)
+ 
+             if (dataObject.usuarioId == 0) {
+                 alert("login incorrecto")
+             } else {
+                 window.location = "ui/listaTarea.HTML";
+             }
+ 
+         })
+         .catch((error) => {
+             console.log(error)
+             alert("Tenemos un error en el Server")
+         })*/
 }
