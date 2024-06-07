2cf32895db102369f2192bb66746ab92eb8f1b5e
mario <mariopolonia0@gmil.com>
2024-06-06 21:11:25 -0400

    agregando el terminado conectado a la api

---
+++ javaScriptBasico/control_tareas_web/script/scriptIndex.js
@@ -0,0 +1,36 @@
+function login() {
+
+    const nombreUsuarioIngresado = document.getElementById('nombreUsuario').value
+    const passwordIngresado = document.getElementById('password').value
+
+    if (nombreUsuarioIngresado == '' || passwordIngresado == '') {
+        alert("Campos estan vacio")
+        return
+    }
+
+    const data = {
+        nombreUsuario: nombreUsuarioIngresado,
+        password: passwordIngresado
+    };
+
+    const uri = "http://www.apinomina.somee.com/Usuario/login";
+
+    fetch(uri, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    })
+        .then((res) => res.json())
+        .then(function (dataObject) {
+
+            if (dataObject.usuarioId == 0) {
+                alert("login incorrecto")
+            } else {
+                window.location = "ui/listaTarea.HTML";
+            }
+
+        })
+        .catch(error => console.log(error))
+}
