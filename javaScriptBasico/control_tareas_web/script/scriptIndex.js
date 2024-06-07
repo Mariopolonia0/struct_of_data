4b564635abe12b51d4c50072164d1f73220fbb0f
mario <mariopolonia0@gmil.com>
2024-06-06 22:45:24 -0400

    agregando el terminado conectado a la api

--- javaScriptBasico/control_tareas_web/script/scriptIndex.js
+++ javaScriptBasico/control_tareas_web/script/scriptIndex.js
@@ -3,50 +3,53 @@ function login() {
     const nombreUsuarioIngresado = document.getElementById('nombreUsuario').value
     const passwordIngresado = document.getElementById('password').value
 
-  /*  if (nombreUsuarioIngresado == '' || passwordIngresado == '') {
+    if (nombreUsuarioIngresado == '' || passwordIngresado == '') {
         alert("Campos estan vacio")
         return
     }
-    
-    */
 
     const data = {
         nombreUsuario: nombreUsuarioIngresado,
         password: passwordIngresado
     };
 
-    //const uri = "http://www.apinomina.somee.com/Usuario/login/";
-    const uri = 'http://www.apinomina.somee.com/api/Books';
+    const uri = 'http://www.apinomina.somee.com/Usuario/login';
 
-    fetch(uri)
-        .then((response) => response.json())
+    fetch(uri, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+        referrerPolicy: "unsafe-url"
+    })
+        .then((res) => res.json())
         .then(function (dataObject) {
-            console.log(dataObject[0].bookId)
-            alert(dataObject[0].nombre)
-        })
-        .catch(error => document.getElementById("username").value = error);
 
-    /* fetch(uri, {
-         method: "POST",
-         headers: {
-             "Content-Type": "application/json",
-         },
-         body: JSON.stringify(data)
-     })
-         .then((res) => res.json())
-         .then(function (dataObject) {
- 
-             alert(dataObject.usuarioId)
- 
-             if (dataObject.usuarioId == 0) {
-                 alert("login incorrecto")
-             } else {
-                 window.location = "ui/listaTarea.HTML";
-             }
- 
-         })
-         .catch((error) => {
-             console.log(error)
-             alert("Tenemos un error en el Server")
-         })*/
+            alert(dataObject.usuarioId)
+
+            if (dataObject.usuarioId == 0) {
+                alert("login incorrecto")
+            } else {
+                window.location = "ui/listaTarea.HTML";
+            }
+
+        })
+        .catch((error) => {
+            console.log(error)
+            alert("Tenemos un error en el Server")
+        })
 }
+
+/*
+const uri = 'http://www.apinomina.somee.com/api/Books';
+
+
+fetch(uri)
+.then((response) => response.json())
+.then(function (dataObject) {
+    console.log(dataObject[0].bookId)
+    alert
+})
+.catch(error => document.getElementById("username").value = error);
+*/
