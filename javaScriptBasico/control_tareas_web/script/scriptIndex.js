0f0c836b32e67037e22eefda806c91387c06ec8f
mario <mariopolonia0@gmil.com>
2024-06-29 15:04:51 -0400

    agregando dialog modal al login

--- javaScriptBasico/control_tareas_web/script/scriptIndex.js
+++ javaScriptBasico/control_tareas_web/script/scriptIndex.js
@@ -1,49 +1,44 @@
 var dialogo = document.getElementById('DialogNotificacion');
 var messageDialog = document.getElementById('messageDialog');
-var html = document.getElementById('html');
 
 dialogo.addEventListener('click', () => dialogo.close());
-html.addEventListener('click', () => dialogo.close());
 
 function login() {
-
     const nombreUsuarioIngresado = document.getElementById('nombreUsuario').value
     const passwordIngresado = document.getElementById('password').value
 
     if (nombreUsuarioIngresado == '' || passwordIngresado == '') {
         messageDialog.innerHTML = "Campos estan vacio";
-        dialogo.show();
-        return
-    }
+        dialogo.showModal();
+    } else {
+        const data = {
+            nombreUsuario: nombreUsuarioIngresado,
+            password: passwordIngresado
+        };
 
-    const data = {
-        nombreUsuario: nombreUsuarioIngresado,
-        password: passwordIngresado
-    };
+        const uri = 'https://controltarea.azurewebsites.net/Usuario/login';
 
-    const uri = 'https://controltarea.azurewebsites.net/Usuario/login';
+        fetch(uri, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        })
+            .then((res) => res.json())
+            .then(function (dataObject) {
 
-    fetch(uri, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data)
-    })
-        .then((res) => res.json())
-        .then(function (dataObject) {
+                if (dataObject.usuarioId == 0) {
+                    messageDialog.innerHTML = "login incorrecto";
+                    dialogo.show();
+                } else {
+                    window.location = "ui/listaTarea.html"; //listaTarea.Html
+                }
 
-            if (dataObject.usuarioId == 0) {
-                messageDialog.innerHTML = "login incorrecto";
+            })
+            .catch((error) => {
+                messageDialog.innerHTML = "Tenemos este error en el Server : " + error;
                 dialogo.show();
-
-            } else {
-                window.location = "ui/listaTarea.html"; //listaTarea.Html
-            }
-
-        })
-        .catch((error) => {
-            messageDialog.innerHTML = "Tenemos este error en el Server : " + error;
-            dialogo.show();
-        })
+            })
+    }
 }
