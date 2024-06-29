ce315a8c9121b82a2c2db57c475b11b51f0bf504
mario <mariopolonia0@gmil.com>
2024-06-29 14:12:54 -0400

    agregando dialog modal al login

--- javaScriptBasico/control_tareas_web/script/scriptIndex.js
+++ javaScriptBasico/control_tareas_web/script/scriptIndex.js
@@ -1,10 +1,18 @@
+var dialogo = document.getElementById('DialogNotificacion');
+var messageDialog = document.getElementById('messageDialog');
+var html = document.getElementById('html');
+
+dialogo.addEventListener('click', () => dialogo.close());
+html.addEventListener('click', () => dialogo.close());
+
 function login() {
 
     const nombreUsuarioIngresado = document.getElementById('nombreUsuario').value
     const passwordIngresado = document.getElementById('password').value
 
     if (nombreUsuarioIngresado == '' || passwordIngresado == '') {
-        alert("Campos estan vacio")
+        messageDialog.innerHTML = "Campos estan vacio";
+        dialogo.show();
         return
     }
 
@@ -26,18 +34,16 @@ function login() {
         .then(function (dataObject) {
 
             if (dataObject.usuarioId == 0) {
-                alert("login incorrecto")
+                messageDialog.innerHTML = "login incorrecto";
+                dialogo.show();
+
             } else {
                 window.location = "ui/listaTarea.html"; //listaTarea.Html
             }
 
         })
         .catch((error) => {
-            console.log(error)
-            alert("Tenemos un error en el Server")
+            messageDialog.innerHTML = "Tenemos este error en el Server : " + error;
+            dialogo.show();
         })
-}
-
-function Registro() {
-
 }
