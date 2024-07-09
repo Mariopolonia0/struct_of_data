7cb6488b01a44104600a510a68bbf40c3077d2e0
mario <mariopolonia0@gmil.com>
2024-07-08 21:27:30 -0400

    agregando el panel para filtrar las tareas

---
+++ javaScriptBasico/control_tareas_web/script/scriptListaTask.js
@@ -0,0 +1,11 @@
+var dialogoFormTask = document.getElementById('DialogFormTask');
+
+//dialogoFormTask.addEventListener('click', () => dialogoFormTask.close());
+
+function formTask() {
+    dialogoFormTask.showModal();
+}
+
+function cancel() {
+    dialogoFormTask.close();
+}
