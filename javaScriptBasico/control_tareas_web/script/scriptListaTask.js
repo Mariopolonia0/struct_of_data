f42fb431d3c72d065a5db638b2aa18a0220ceebe
mario <mariopolonia0@gmil.com>
2024-08-03 21:32:42 -0400

    agregando registro de tareas

--- javaScriptBasico/control_tareas_web/script/scriptListaTask.js
+++ javaScriptBasico/control_tareas_web/script/scriptListaTask.js
@@ -1,7 +1,8 @@
 var dialogoFormTask = document.getElementById('DialogFormTask');
 var dialogoBuscarUsetTask = document.getElementById('DialogBuscarUser');
-//dialogoFormTask.addEventListener('click', () => dialogoFormTask.close());
+const contenedorCarta = document.getElementById('listCard');
 
+dialogoBuscarUsetTask.onpen
 function formTask() {
     dialogoFormTask.showModal();
 }
@@ -11,6 +12,7 @@ function cancel() {
 }
 
 function buscarUserForTask() {
+    getUserForTask();
     dialogoBuscarUsetTask.showModal();
 }
 
@@ -20,11 +22,50 @@ function cancelBuscarUserForTask() {
 }
 
 
-function clean(){
+function clean() {
 
 }
 
 
-function savedata(){
+function savedata() {
 
+}
+
+function getUserForTask() {
+    //https://controltarea.azurewebsites.net/Usuario/SP
+
+    const uri = 'https://controltarea.azurewebsites.net/Usuario/SP';
+    contenedorCarta.innerHTML = ` 
+          
+        `
+
+    fetch(uri, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        }
+    })
+        .then((res) => res.json())
+        .then(function (dataObject) {
+            dataObject.forEach(element => llenarListaUsuarioForTask(element))
+        })
+        .catch((error) => {
+            console.log(error)
+            alert("Tenemos un error en el Server")
+        })
+}
+
+function llenarListaUsuarioForTask(element) {
+
+
+    contenedorCarta.innerHTML += ` 
+    <div class="cardItemUser" onclick="probando(${element.usuarioId})">
+        <h3 class="card-title">${element.nombreAndApellido}</h3>
+    </div>     
+    `
+
+}
+
+function probando(numero){
+    alert(numero);
 }
