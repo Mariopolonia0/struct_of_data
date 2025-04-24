7e8ad60a9f8ba7f9fe3d34efe8e880b348c24a75
Mario Peña Polonia <65502311+Mariopolonia0@users.noreply.github.com>
2025-04-23 20:21:14 -0400

    Create codeAcademy.kt

---
+++ EjercicioKotlin/codeAcademy.kt
@@ -0,0 +1,25 @@
+fun main() {
+
+  /*
+  Take a look at some of the following escape sequences and their operations:
+   \n Inserts a new line
+   \t Inserts a tab
+   \r Inserts a carriage return
+   \' Inserts a single quote
+   \" Inserts a double quote
+   \\ Inserts a backslash
+   \$ Inserts the dollar symbol
+   */
+/*
+Dear Learner 👋,
+
+I hope you're enjoying the Learn Kotlin course. 
+You've made tremendous progress. Keep going! 
+And remember, "Knowledge is power." (Frances Beacon). 
+
+With love, 
+Codey
+
+*/
+  print("Dear Learner 👋,\n\nI hope you're enjoying the Learn Kotlin course.\nYou've made tremendous progress. Keep going!\nAnd remember, \"Knowledge is power.\" (Frances Beacon). \n\nWith love, \nCodey")
+}
